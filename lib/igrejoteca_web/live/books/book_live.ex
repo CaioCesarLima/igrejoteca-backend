dc1e2c65d275ef98c54e8e67e4240ed1bb4f793e
caioquleap <caio@quleap.com>
2023-05-25 23:27:02 -0300

    remove liveviews

--- lib/igrejoteca_web/live/books/book_live.ex
+++
@@ -1,7 +0,0 @@
-defmodule IgrejotecaWeb.BookLive do
-  use Phoenix.LiveView
-
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, message: "Estamos enfrentando algum problema")}
-  end
-end
--- lib/igrejoteca_web/live/error_live.ex
+++
@@ -1,9 +0,0 @@
-defmodule IgrejotecaWeb.ErrorLive do
-  use Phoenix.LiveView
-
-
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, message: "Estamos enfrentando algum problema")}
-  end
-
-end
