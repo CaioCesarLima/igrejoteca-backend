2fc2b8a742de5bff1e62d7b02cd47f229b66ae60
caioquleap <caio@quleap.com>
2023-10-02 17:07:26 -0300

    translate errors

--- lib/igrejoteca_web/utils/translate.ex
+++ lib/igrejoteca_web/utils/translate.ex
@@ -11,12 +11,9 @@ defmodule IgrejotecaWeb.Utils.Translate do
   }
 
   def translate_error(error_message, locale \\ "en") do
-    IO.inspect(locale, label: "Locale")
-    IO.inspect(error_message, label: "error_message")
-    IO.inspect(@translations, label: "Translations")
     IO.inspect(Map.fetch!(@translations, locale), label: "map fetch")
     case Map.fetch!(@translations, locale) do
-      {_, translations} ->
+      %{_, translations} ->
         Map.get(translations, error_message, error_message)
       erro ->
         error_message
